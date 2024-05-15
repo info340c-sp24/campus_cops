e53e6ff09f7808fb112ae75ce594289202d9e773
DevDhawan <devdhawan2004@gmail.com>
2024-05-14 17:56:30 -0700

    Done with safteyresources
    components:
    Navbar
    Header
    Footer
    
    Add file into app folder:
    Saftey Resources

---
+++ draft-2/src/app/SafteyResources.tsx
@@ -0,0 +1,69 @@
+import Head from "./components/Head";
+import Footer from "./components/Footer";
+
+import uwNightShuttle from "../assets/uw-night-shuttle.png";
+
+function SafteyResources() {
+  return (
+    <>
+      <Head />
+      <main>
+        <section>
+          <div className="container">
+            <img
+              src={uwNightShuttle}
+              alt="Map of the University of Washington night shuttle pick up location in West and North campus."
+              width="500"
+            />
+            <h2>Emergency Contacts</h2>
+            <ul>
+              <li>Police Department 911</li>
+              <li>
+                <strong>Non-Emergency Police:</strong> (206) 685-8973
+              </li>
+              <li>
+                <strong>Emergency Management:</strong> (206) 897-8000
+              </li>
+            </ul>
+            <h2>Campus Safety Resources</h2>
+            <p>
+              Here are some additional resources for staying safe on campus:
+            </p>
+            <ul>
+              <li>
+                <strong>UW Police Department:</strong>
+                <a target="_blank" href="https://police.uw.edu/">
+                  https://police.uw.edu/
+                </a>
+              </li>
+              <li>
+                <strong>Emergency Management:</strong>
+                <a target="_blank" href="https://www.washington.edu/emergency/">
+                  https://www.washington.edu/emergency/
+                </a>
+              </li>
+              <li>
+                <strong>Husky NightWalk:</strong> Provides walking escorts on
+                campus after dark. Call (206) 685-WALK (9255) or use the Husky
+                NightWalk app.
+              </li>
+              <li>
+                <strong>SafeCampus:</strong> Confidential advocacy and support
+                for safety concerns. Call (206) 685-SAFE (7233) or visit
+                <a
+                  target="_blank"
+                  href="https://www.washington.edu/safecampus/"
+                >
+                  https://www.washington.edu/safecampus/
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default SafteyResources;
---
+++ draft-2/src/app/components/Head.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+function Head() {
+  return (
+    <header>
+      <NavBar />
+      <h1>University of Washington Safety Resources</h1>
+    </header>
+  );
+}
+
+export default Head;
