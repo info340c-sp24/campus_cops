44fe717ffc0c83ba1b94d25d954840e80b9aeff1
Charlene Chang <charlenechang@Charlenes-MacBook-Pro-2.local>
2024-06-03 16:55:08 -0700

    update

--- draft-2/src/app/Welcome.tsx
+++ draft-2/src/app/Welcome.tsx
@@ -1,31 +1,8 @@
 import uwRedSquare from "../assets/uw-red-square.avif";
 import uwCampusMap from "../assets/UW-Campus-Map.png";
-
 function Welcome() {
   return (
     <>
-      <header className="header">
-        <div className="header-content">
-          <img src={logo} alt="UW SafeRoute Logo" className="logo" />
-          <h1>Campus Cops</h1>
-        </div>
-      </header>
-
-      <main>
-        <div className="container">
-          <section className="intro">
-            <h1>Welcome to UW Campus Cops</h1>
-            <img src={uwRedSquare} alt="UW Red Square during cherry blossom" width="550" className="intro-image" />
-            <cite>
-              <a href="https://unsplash.com/photos/people-walking-on-street-during-daytime-cI2_bHXCzRg" target="_blank" rel="noopener noreferrer">
-                Image by Eunice Choi
-              </a>
-            </cite>
-            <p>
-              Welcome to UW Campus Cops, your dedicated safety companion on the University of Washington (UW) Seattle campus. We understand the importance of campus safety and have developed this platform to enhance the well-being and security of all students, faculty, and staff. Our app is designed to address various safety challenges, including theft, assault, and navigation in poorly lit or unfamiliar areas.
-            </p>
-          </section>
-        </div>
       <main>
         <div className="container">
           <div className="content">
@@ -52,9 +29,7 @@ function Welcome() {
               unfamiliar areas.
             </p>
           </div>
-
-        <div className="container">
-          <section className="features">
+          <div className="content">
             <h2>Discover Safe Routes, Navigate with Confidence</h2>
             <h3>Explore our Features</h3>
             <p>Real-time Safety Alerts, Interactive Safety Maps, and More.</p>
@@ -64,15 +39,10 @@ function Welcome() {
               <li>Campus Safety Resources</li>
               <li>Community Based Incident Forums</li>
             </ul>
-          </section>
+          </div>
         </div>
       </main>
-
-      <footer>
-        <p>&copy; 2024 UW Campus Cops. All rights reserved.</p>
-      </footer>
     </>
   );
 }
-
 export default Welcome;
