e5381f11eacf3305b0802b0cca6e500f51a01a56
sophia-mai <sophm23@uw.edu>
2024-06-03 17:58:05 -0700

    chat changes

--- draft-2/src/app/CommunityChat.tsx
+++ draft-2/src/app/CommunityChat.tsx
@@ -30,16 +30,6 @@ function CommunityChat(props) {
         addMessageFunction={addMessage}
         />
     </div>
-    <>
-      <main>
-        <h1>UW Seattle Community Chat Forum</h1>
-        <div className="row flex-grow-1">
-          <div className="col d-flex flex-column">
-            <ChatPane />
-          </div>
-        </div>
-      </main>
-    </>
   );
 }
 
